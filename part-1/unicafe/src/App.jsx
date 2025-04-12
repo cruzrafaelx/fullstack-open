ee03780035257f3051f29d755935f4f5b4ba1237
Rafael Cruz <rafaelcruz@outlook.de>
2025-04-12 06:13:58 +0200

    Add tables and finish logic

--- part-1/unicafe/src/App.jsx
+++ part-1/unicafe/src/App.jsx
@@ -1,19 +1,22 @@
 import { useState } from 'react'
 import './App.css'
 
-function Button({text}){
+function Button({text, onClick}){
   return(
-    <button>
-      {text}
-    </button>
+    <td>
+      <button onClick={onClick}>
+        {text}
+      </button>
+    </td>
   )
 }
 
-function Statistics({text}){
+function Statistics({text, value}){
   return(
-    <div>
-      {text}
-    </div>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
@@ -22,25 +25,62 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
-  const handleGood = () => setGood(good + 1)
-  const handleNeutral = () => setNeutral(neutral + 1)
-  const handleBad = () => setBad(bad + 1)
+  const handleGood = () => {
+    setGood(good + 1)
+    setAll(all + 1)
+  }
+  
+  const handleNeutral = () => {
+    setNeutral(neutral + 1)
+    setAll(all + 1)
+  }
+
+  const handleBad = () => {
+    setBad(bad + 1)
+    setAll(all + 1)
+  }
+
+  console.log(good, neutral, bad, all)
 
   return (
-    <div>
-      <h1>Give Feedback</h1>
-      <Button text="good" onClick={handleGood} />
-      <Button text="neutral" onClick={handleNeutral}/>
-      <Button text="bad" onClick={handleBad}/>
-      <h1>Statistics</h1>
-      <Statistics text="good" value={good}/>
-      <Statistics text="neutral" value={neutral}/>
-      <Statistics text="bad" value={bad}/>
-      <Statistics text="all" value={good + neutral + bad}/>
-      <Statistics text="average" value={(good + neutral + bad) / 3}/>
-      <Statistics text="positive" value={good}/>
-    </div>
+    <table>
+      <tbody>
+      <tr>
+        <td colSpan={3}>
+          <h1>give feedback</h1>
+        </td>
+      </tr>
+
+      <tr>
+          <Button text="good" onClick={handleGood} />
+          <Button text="neutral" onClick={handleNeutral}/>
+          <Button text="bad" onClick={handleBad}/>
+      </tr>
+      
+      <tr>
+        <td colSpan={3}>
+          <h1>statistics</h1>
+        </td>
+      </tr>
+
+      {all ? (
+        <>
+        <Statistics text="good" value={good}/>
+        <Statistics text="neutral" value={neutral}/>
+        <Statistics text="bad" value={bad}/>
+        <Statistics text="all" value={all}/>
+        <Statistics text="average" value={parseFloat((good - bad )/ all).toFixed(2)}/>
+        <Statistics text="positive" value={`${parseFloat(good / all).toFixed(2)}%`}/>
+        </>
+      ) : (
+        <tr>
+          <td colSpan={2}>No feedback given</td>
+        </tr>
+      )}
+      </tbody>
+    </table>
   )
 }
 
