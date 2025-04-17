0225a461a15a72ab1ac8d891191f159f2cbb67de
Rafael Cruz <rafaelcruz@outlook.de>
2025-04-17 06:15:42 +0200

    Finish exercise 2.6

--- part-2/phonebook/phonebook/src/App.jsx
+++ part-2/phonebook/phonebook/src/App.jsx
@@ -8,12 +8,22 @@ function App() {
     ]) 
     const [newName, setNewName] = useState('')
 
+    const handleSubmit = (event) => {
+      event.preventDefault()
+      const newPerson = [...persons, {name: newName}]
+      setPersons(newPerson)
+    }
+
+    const handleChange = (event) => {
+      setNewName(event.target.value)
+    }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
-          name: <input />
+          name: <input onChange={handleChange} value={newName}/>
         </div>
         <div>
           <button type="submit">add</button>
