cc82fb69b7262775ce4d170831b63c3c7069e7a4
Rafael Cruz <rafaelcruz@outlook.de>
2025-05-12 04:53:08 +0200

    Add display country function

--- part-2/countries/src/App.jsx
+++ part-2/countries/src/App.jsx
@@ -4,8 +4,10 @@ import axios from 'axios'
 
 function App() {
   
-  const [countries, setCountries] = useState(null)
-  const [value, setValue] = useState("Type a country...")
+  const [countries, setCountries] = useState([])
+  const [value, setValue] = useState("")
+  const [countryFilter, setCountryFilter] = useState([])
+  const [displayCountries, setDisplayCountries] = useState([])
 
   useEffect(()=>{
     axios
@@ -21,9 +23,83 @@ function App() {
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    console.log(value)
   }
+
+  useEffect(()=>{
+    filterCountry()
+  }, [value])
+
+  //Renders the country depending on the given conditions
+  const displayCountry = () => {
+    
+    if(countryFilter.length > 0) {
+
+      //Displays a caution if there are more than 10 countries found
+      if(countryFilter.length >= 10){
+        return <p>Please enter a more specific filter</p>
+      }
+      
+      //Displays country names together with show button if  1 < country > 10
+      else if(countryFilter.length > 1 && countryFilter.length < 10){
+        return countryFilter.map(country => {
+          return (
+          <div>
+            <p>{country.name.common}</p>
+            <button>Show more</button>
+          </div>
+          )
+        })
+      }
+      
+      //Displays country details if there is only 1 match
+      else if(countryFilter.length === 1) {
+        return (
+          <div>
+            <h1>{countryFilter[0].name.common}</h1>
+            <p>Capital: {countryFilter[0].capital[0]}</p>
+            <h2>Languages</h2>
+            <ul>
+              {Object.keys(countryFilter[0].languages).map(key => {
+                return <li key={key}>{countryFilter[0].languages[key]}</li>
+              })}
+            </ul>
+            <img src={countryFilter[0].flags.png} alt={`${countryFilter[0].name.common} flag`}/>
+          </div>
+        )
+      }
+    }
+    
+    return null
+  }
+
+  console.log(countryFilter)
   
+  //Make a filter handler that filters all the countries depending on the value 
+  const filterCountry = () =>{
+    if(countries.length > 0){
+
+      //Displays the name of the country if there is an exact match
+      const exactMatch = countries.find(country => {
+        const countryName = country.name.common
+        return countryName.toLowerCase() === value.toLowerCase()
+      })
+
+      console.log("exact match: ", exactMatch)
+      
+      if(exactMatch){
+        setCountryFilter([exactMatch]) 
+      }
+      
+      else{
+        const filteredCountries = countries.filter(country => {
+          const countryName = country.name.common
+          return countryName.toLowerCase().includes(value.toLowerCase())
+        })  
+        setCountryFilter(filteredCountries)
+      }
+    }
+  }
+
 
   return (
     <div>
@@ -32,6 +108,9 @@ function App() {
         <input value={value} onChange={handleChange}></input>
         <button type='submit'>Submit</button>
       </form>
+      <div>
+        {displayCountry()}
+      </div>
     </div>
   )
 }
