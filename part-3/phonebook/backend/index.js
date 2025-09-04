f335269b38c246d23973491a005e38e69e826da5
Rafael Cruz <rafaelcruz@outlook.de>
2025-09-04 17:41:18 +0200

    Add enivorment variable PORT to backend

--- part-3/phonebook/backend/index.js
+++ part-3/phonebook/backend/index.js
@@ -118,7 +118,7 @@ app.post('/api/phonebook', (request, response) => {
 })
 
 //Create server
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
