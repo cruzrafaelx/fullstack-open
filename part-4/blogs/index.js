4c67d11cbc33f3cd294728e329e5559a7f966707
Rafael Cruz <rafaelcruz@outlook.de>
2025-09-11 14:15:42 +0200

    Seperate the code into its own modules

--- part-4/blogs/index.js
+++ part-4/blogs/index.js
@@ -1,1 +1,6 @@
-console.log('Initial commit')
+const app = require('./app')
+const config = require('./utils/config')
+
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`)
+})
---
+++ part-4/blogs/utils/config.js
@@ -0,0 +1,6 @@
+require('dotenv').config()
+
+const PORT = process.env.PORT
+const MONGODB_URI = process.env.MONGODB_URI
+
+module.exports = { PORT, MONGODB_URI }
